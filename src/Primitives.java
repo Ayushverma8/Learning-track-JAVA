9d229d7c7d8c9d99e1d931eb3b32acbdff106aea
Ayush Verma <ay322266@dal.ca>
2022-10-31 21:32:15 -0300

    Added Primitives

---
+++ src/Primitives.java
@@ -0,0 +1,12 @@
+public class Primitives {
+    public static void main(String[] args){
+//        Always add L at the end to avoid confusion to JAVA. Also add underscore after every third digit in JAVA
+        long temperature = 3_334_343_432L;
+//        Add F at the last in case you are declaring the float variables.
+        float price = 4.334F;
+//        Always use single quote for single character and multiple quote for multiple characters
+        char alphabet = 'C';
+        System.out.println(temperature);
+        boolean isEligible = false;
+    }
+}
